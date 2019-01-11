3dcd12f023a4c73f55056cd1469af3be04b68602
Sanket Timsina <jhapallisanket@gmail.com>
2019-01-11 11:02:12 +0545

    Made few improvements

---
+++ CheNumSolver/CheNumSolver/Quantities/Volume.vb
@@ -0,0 +1,9 @@
+﻿Public Class Volume
+    Private ReadOnly lITRES_PER_METERCUBED = 1000
+    Private ReadOnly CMCUBED_PER_METERCUBED = 1000000
+    Private Litre As Double
+    Private MeterCubed As Double
+    Private Sub SetMeterCubedFromLitre(ByVal Litre As Double)
+
+    End Sub
+End Class
