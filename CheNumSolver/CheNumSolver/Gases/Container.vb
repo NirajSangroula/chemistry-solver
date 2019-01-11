93d91838b36d2e1d7ea80de02c3f16cccb9e0eb3
Sanket Timsina <jhapallisanket@gmail.com>
2019-01-11 16:13:11 +0545

    I just did something

--- CheNumSolver/CheNumSolver/Gases/Container.vb
+++ CheNumSolver/CheNumSolver/Gases/Container.vb
@@ -1,3 +1,7 @@
 ﻿Public Class Container
-
+    Private Pressure As Pressure
+    Private Volume As Volume
+    Private Temperature As Temperature
+    Property DiffusionTime As Double
+    Property Gas() As Gas
 End Class
